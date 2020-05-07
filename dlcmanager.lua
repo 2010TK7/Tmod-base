4e9620052569707467d6721ba162abbc7c16d869
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-05-07 15:21:37 +0800

    200301

--- dlcmanager.lua
+++
@@ -1,4 +0,0 @@
-local OldFunc = WINDLCManager._check_dlc_data
-function WINDLCManager:_check_dlc_data(dlc_data)
-	return OfflineSource and dlc_data.source_id and OfflineSource:Checker(dlc_data.source_id) or OldFunc(self, dlc_data)
-end
--- missionmanager.lua
+++
@@ -1,7 +0,0 @@
-local OldFunc = MissionManager.init
-function MissionManager:init(...)
-	OldFunc(self, ...)
-	if SpawnSupporter then
-		SpawnSupporter:Reload()
-	end
-end
