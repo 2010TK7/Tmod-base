3d75ba25dbcdae90128f0174f871576d54415b16
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-05-07 15:20:58 +0800

    190614

--- playertweakdata.lua
+++ playertweakdata.lua
@@ -2,7 +2,7 @@ local OldFunc = PlayerTweakData.init
 
 function PlayerTweakData:init()
 	OldFunc(self)
-	if CommonModifiers and CommonModifiers.checker then
+	if CommonModifiers and CommonModifiers.Checker then
 		CommonModifiers:LessPagers(self.alarm_pager.bluff_success_chance)
 	end
 end
